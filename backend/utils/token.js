a4b85813cef1ec27b5959f9ea568679f3750e0a9
rohit <rohitsb96134@gmail.com>
2025-01-19 21:07:01 +0530

    add token generation utility for JWT authentication

---
+++ backend/utils/token.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+
+export const generateToken = ({res, userId}) => {
+    const token = jwt.sign({userId}, process.env.JWT_SECRET,
+         {expiresIn: '7d'});
+         res.cookie('access_token', token, {
+             httpOnly: true,
+             sameSite: true,
+             maxAge: 7*24*60*60*1000
+         }
+
+         );
+}
