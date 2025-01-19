dd141307c25e255a7b4ea01674cfd1f716af5cf9
rohit <rohitsb96134@gmail.com>
2025-01-20 00:43:11 +0530

    enhance message model to reference user IDs for sender and receiver

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -4,11 +4,13 @@ import mongioose from 'mongoose';
 const messageSchema = new mongoose.Schema({
     senderId:{
         type: mongoose.Schema.Types.ObjectId,
-        required: true
+        required: true,
+        ref: 'User'
     },
     receiverId:{
         type: mongoose.Schema.Types.ObjectId,
-        required: true
+        required: true,
+        ref: 'User'
     },
     message:{
         type: String,
