b6dd2af856ccfb4a5ce529dc3899b52c2d40a1cc
rohit <rohitsb96134@gmail.com>
2025-01-21 14:04:02 +0530

    fix: enhance sendMessage function to create conversation if it doesn't exist and save messages concurrently

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,17 +1,47 @@
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
 
+const sendMessage = async (req, res) => {
+  try {
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user.id;
 
- const sendMessage = async (req, res) => {
-    try {
-        const {message}  = req.body;
-        const {id : receiverId} = req.params;
-        const senderId = req.user.id;
+    let conversation = await Conversation.findOne({
+      participants: {
+        $all: [senderId, receiverId],
+      },
+    });
 
-
-    } catch (error) {
-        console.log("error in send Message Controller");
-        
-        res.status(500).json({ error : "internal server error" });
+    if (!conversation) {
+      conversation = await Conversation({
+        participants: [senderId, receiverId],
+      });
     }
-}
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
+
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
+    }
+// this runs in the background
+    // await conversation.save();
+    // await newMessage.save();
+
+    // this runs in parallel
+
+    await Promise.all([conversation.save(), newMessage.save()]);
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("error in send Message Controller");
+
+    res.status(500).json({ error: "internal server error" });
+  }
+};
 
 export default sendMessage;
