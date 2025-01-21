d3cac12826a0c48fb0e818f0c451fd64ebfe078e
rohit <rohitsb96134@gmail.com>
2025-01-21 15:50:59 +0530

    fix: enhance getSidebarUsers controller to handle errors and filter out logged user

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,15 @@
 export const getSidebarUsers = async (req, res) => {
-    
+    try {
+        
+        const loggedUserID = req.user._id;
+
+        const filterUsers = await User.find({ _id: { $ne: loggedUserID } });
+res.status(200).json(filterUsers);
+
+    } catch (error) {
+        console.log("Error in getSidebarUsers controller: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+        
+    }
 }
     
