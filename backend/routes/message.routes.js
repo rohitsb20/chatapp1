b60523ed6c1aabf9f3201281711dd8d6b729f279
rohit <rohitsb96134@gmail.com>
2025-01-21 15:05:02 +0530

    add: implement GET route for retrieving messages by conversation ID

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -3,6 +3,7 @@ import sendMessage from '../controllers/message.controller.js';
 import protectRoute  from '../middleware/protectRoute.js';
 const router = express.Router();
 
+router.get("/:id", getMessages);
 router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
