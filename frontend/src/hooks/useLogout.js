890b186102c5392f5370f06efc41d9d4e3bb8151
rohit <rohitsb96134@gmail.com>
2025-01-23 16:48:44 +0530

    feat: add custom logout hook with error handling and loading state

---
+++ frontend/src/hooks/useLogout.js
@@ -0,0 +1,43 @@
+
+import toast from 'react-hot-toast';
+import { useAuthContext } from '../context/AuthContext';
+import { useState } from 'react';
+
+
+const useLogout =  () => {
+const [loading , setLoading] = useState(false);
+
+const {setAuthUser} = useAuthContext();
+
+const logout = async () => {
+    try {
+      const res = await fetch("/api/auth/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await res.json();
+        if(data.error) {
+            throw new Error(data.error)
+        }
+        localStorage.removeItem("chat-user");
+        setAuthUser(null);
+        toast.success("Logout successful");
+    } catch (error) {
+    toast.error(error.message);
+
+    }
+    finally {
+        setLoading(false);
+    }
+
+  
+}
+
+
+  return { logout, loading };
+}
+
+export default useLogout;
