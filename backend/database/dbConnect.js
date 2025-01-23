9927204bf5f40ec2580b64efb2ca4d40d1a4b769
rohit <rohitsb96134@gmail.com>
2025-01-23 16:47:07 +0530

    fix : database connecting error

--- backend/database/dbConnect.js
+++ backend/database/dbConnect.js
@@ -3,11 +3,12 @@ import mongoose from "mongoose";
 
 const dbconnect = async () => {
     try {
-      await  mongoose.connect(process.env.MONGO_URI);
+      await mongoose.connect(process.env.MONGO_URI);
         console.log("Mongo db is connected");
         
     } catch (error) {
         console.log("Error connecting to database");
+        console.log(error);
         
     }
 }
