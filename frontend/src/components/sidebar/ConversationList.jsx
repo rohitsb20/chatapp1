8ce16375a13d904c213023f121fe361561745d54
rohit <rohitsb96134@gmail.com>
2025-01-23 18:00:33 +0530

    feat: integrate zustand for conversation state management and implement useGetConversation hook

--- frontend/src/components/sidebar/ConversationList.jsx
+++ frontend/src/components/sidebar/ConversationList.jsx
@@ -1,19 +1,22 @@
+/* eslint-disable react/prop-types */
 
 
-export default function ConversationList() {
+export default function ConversationList({ conversation, lastindex }) {
   return (
-    <div className="flex items-center space-x-2 ">
-      <div className="w-12 h-12">
-        <img
-          src="/images/avatar.png"
-          alt="avatar"
-          className="w-12 h-12  border-2 rounded-full"
-        />
+    <>
+      <div className="flex items-center space-x-2 ">
+        <div className="w-12 h-12">
+          <img
+            src={conversation.profilePicture}
+            alt="avatar"
+            className="w-12 h-12  border-2 rounded-full"
+          />
+        </div>
+        <div className=" space-x-2 hidden md:flex items-center justify-between">
+          <h3 className="text-sm font-semibold">{conversation.username}</h3>
+        </div>
       </div>
-      <div className=" space-x-2 hidden md:flex items-center justify-between">
-        <h3 className="text-sm font-semibold">John Doe</h3>
-        <p className="text-gray-500 text-sm">Hey</p>
-      </div>
-    </div>
+      {!lastindex && <div className="divider my-0 py-0 h-1" />}
+    </>
   );
 }
--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -2,31 +2,30 @@
 import Searchbox from "../Searchbox";
 import ConversationList from "../sidebar/ConversationList";
 import LogoutButton from "../LogoutButton";
+import useGetConversation from "../../hooks/useGetConversation";
 
 export default function Sidebar() {
-  return (
-    <div className="border-r-2 md:w-1/4  p-4 ">
-      <div className="md:block hidden">
-        <Searchbox />
-        <hr className="my-4 md:block hidden" />
-      </div>
+      const {conversations, loading} = useGetConversation()
+ console.log(conversations);
+ 
+      return (
+        <div className="border-r-2 md:w-1/4  p-4 ">
+          <div className="md:block hidden">
+            <Searchbox />
+            <hr className="my-4 md:block hidden" />
+          </div>
 
-      <div className="flex flex-col gap-y-4  ">
-        <div className="flex justify-center   md:border-2 p-2 rounded-xl ">
-          <ConversationList />
-        </div>
-        <div className="flex justify-center md:border-2 p-2 rounded-xl">
-          <ConversationList />
-        </div>
-        <div className="flex justify-center md:border-2 p-2 rounded-xl">
-          <ConversationList />
-        </div>
-        <div className="flex justify-center md:border-2 p-2 rounded-xl">
-          <ConversationList />
-        </div>
-      </div>
+          <div className="flex flex-col gap-y-4  ">
+            {conversations.map((conversation) => (
+              <div key={conversation._id} className="flex justify-center md:border-2 p-2 rounded-xl">
+                <ConversationList  conversation={conversation} lastindex={conversations.length - 1}/>
+              </div>
+            ))}
+          </div>
 
-      <LogoutButton />
-    </div>
-  );
+          {loading ? <span className="loading loading-spinner"></span> : null}
+
+          <LogoutButton />
+        </div>
+      );
 }
