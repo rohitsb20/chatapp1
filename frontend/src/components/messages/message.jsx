45ee6c2a88ec05cf2c2c02b236a924eff1147c3d
rohit <rohitsb96134@gmail.com>
2025-01-24 16:03:32 +0530

    feat: refactor Message component layout and styles for improved UI

--- frontend/src/components/messages/message.jsx
+++ frontend/src/components/messages/message.jsx
@@ -9,21 +9,28 @@ const Message = ({ message }) => {
 	const fromMe = message.senderId === authUser._id;
 
 	const chatClassName = fromMe ? "chat-end" : "chat-start";
-	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+	const profilePic = fromMe ? authUser.profilePicture : selectedConversation?.profilePicture;
 	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
 
 	const shakeClass = message.shouldShake ? "shake" : "";
 
 	return (
-		<div className={`chat ${chatClassName}`}>
-			<div className='chat-image avatar'>
-				<div className='w-10 rounded-full'>
-					<img alt='Tailwind CSS chat bubble component' src={profilePic} />
-				</div>
-			</div>
-			<div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
-			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>12:45</div>
-		</div>
-	);
+    <div className={`chat ${chatClassName}`}>
+      <div className="chat-image avatar ">
+        <div className="w-full rounded-full  flex items-center gap-x-4 ">
+          <img alt="Tailwind CSS chat bubble component" className="w-10" src={profilePic} />
+          <div
+            className={`chat-bubble text-black  ${bubbleBgColor} ${shakeClass} pb-2`}
+          >
+            {message.message}
+          </div>
+        </div>
+      </div>
+
+      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
+        12:45
+      </div>
+    </div>
+  );
 };
 export default Message;
