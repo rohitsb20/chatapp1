2ec17fa3c7d9cb86c2b0cc1ca10a755e11b34088
rohit <rohitsb96134@gmail.com>
2025-01-24 11:51:54 +0530

    feat: add useMessages hook for sending messages with loading state management

---
+++ frontend/src/hooks/useMessages.js
@@ -0,0 +1,41 @@
+import useState from 'react';
+import toast from 'react-hot-toast';
+import { useConversation } from './useConversation';
+
+
+export const useMessages = () => {
+  const [loading, setLoading] = useState(true);
+    const {messages, setMessages, selectedConversation} =  useConversation();
+
+
+    const sendMessage = async (message) => {
+
+        setLoading(true);
+        try {
+            const response = await fetch(`api/messages/send/${selectedConversation._id} `, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ message }),
+            });
+
+            const data = await response.json();
+            if(data.error){
+             throw new Error(data.error);
+            }
+
+            setMessages([...messages, data]);
+            
+        } catch (error) {
+            toast.error('Error sending message', error);
+        } finally{
+            setLoading(false);
+        }
+
+        
+    }
+
+ return { loading, sendMessage };   
+}
+
