c4aa850fc6d3685dcafde3dca889e2708a9239e9
rohit <rohitsb96134@gmail.com>
2025-01-25 01:11:36 +0530

    feat: enhance SearchInput component with improved search validation and user feedback

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -5,24 +5,28 @@ import { toast } from "react-hot-toast";
 import { useConversation } from "../../zustand/useConversation";
 import { useGetConversation } from '../../hooks/useGetConversation';
 const SearchInput = () => {
+const [search, setSearch] = useState("");
+const { setSelectedConversation } = useConversation();
+const { conversations } = useGetConversation();
 
-  const [search, setSearch] = useState("");
-  const {setSelectedConversation} = useConversation(); 
-  const  {conversations} = useGetConversation();
-    const handleSubmit = (e) => {
-    e.preventDefault();
-      if(!search) return; 
-      if(search < 3) 
-      return toast.error("Search term must be at least 3 characters long");
-
-      const conversation = conversations.find((c) => c.username.toLowerCase()
-      .includes(search.toLowerCase()));
-
-      if(!conversation) return toast.error("No user found");
-      setSelectedConversation(conversation);
-      setSearch("");
+const handleSubmit = (e) => {
+  e.preventDefault();
+  if (!search) return;
+  if (search.length < 3) {
+    return toast.error("Search term must be at least 3 characters long");
   }
-  
+
+  const conversation = conversations.find((c) =>
+    c.fullName.toLowerCase().includes(search.toLowerCase())
+  );
+
+  if (conversation) {
+    setSelectedConversation(conversation);
+    setSearch("");
+  } else toast.error("No such user found!");
+};
+
+
 
 	return (
     <div >
