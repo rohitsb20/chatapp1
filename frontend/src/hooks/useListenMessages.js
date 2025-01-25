e1e1bce62ca27bdc39df0119f57c03d91ab221e6
rohit <rohitsb96134@gmail.com>
2025-01-25 13:13:27 +0530

    fix: add audio notification for new messages in useListenMessages hook

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -1,6 +1,7 @@
 import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversation";
 import { useEffect } from "react";
+import oh_no from '../assets/sounds/oh_no.mp3';
 
 const useListenMessages = () => {
   const { socket } = useSocketContext();
@@ -8,6 +9,8 @@ const useListenMessages = () => {
   useEffect(() => {
     socket?.on("newMessage", (newMessage) => {
       newMessage.shouldShake = true;
+      const audio = new Audio(oh_no);
+      audio.play();
       setMessages([...messages, newMessage]);
     });
     return () => socket?.off("newMessage");
