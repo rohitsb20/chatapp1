75a12481f6d9fca2041acbc8d2a495e7a40ee2c7
rohit <rohitsb96134@gmail.com>
2025-01-25 09:57:25 +0530

    feat: refactor server setup to integrate Socket.IO by moving app initialization to socket.js

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,26 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import dbconnect from './database/dbConnect.js';
-import authRoutes from './routes/auth.routes.js';
-import messageRoutes from './routes/message.routes.js';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import dbconnect from "./database/dbConnect.js";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 // const app = express();  moving in socket io and importing it from socket.js
-import {app} from './socket/socket.js'; // importing app from socket.js
+import { app, server } from "./socket/socket.js"; // importing app from socket.js
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 
-
-
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+// app.listen(3000, () => { //updating to use socket.io server
+//     dbconnect();
+//     console.log(`Server is running on port 3000`);
+// });
 
-app.listen(3000, () => {
-    dbconnect();
-    console.log(`Server is running on port 3000`);
+server.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
 });
--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,13 +1,31 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
-
-
 const app = express();
 
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST']
+    }
+});
+
+io.on('connection', (socket) => {
+
+
+    console.log('a user connected', server.id);
+
+
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected', server.id);
+    });
+    
 
 
 
 
+});
 
-export {app}; // exporting app to be used in server.js
+export {app, io, server}; // exporting app, io, server to be used in server.js
